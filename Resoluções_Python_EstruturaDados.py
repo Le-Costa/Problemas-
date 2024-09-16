15d65336fff574ede9669579a129433086086620
Leticia Costa <leticia.k.f.dacosta@gmail.com>
2024-09-16 13:47:23 -0300

    16092024_1347

---
+++ Resoluções_Python_EstruturaDados.py
@@ -0,0 +1,26 @@
+# Exercício 1 - Imprima na tela os números de 1 a 10. Use uma lista para armazenar os números.
+numeros = [1,2,3,4,5,6,7,8,9,10]
+print (numeros)
+
+# Exercício 2 - Crie uma lista de 5 objetos e imprima na tela
+objetos= ['mouse','teclado','Fone','CPU', 'Apoio'] 
+print (objetos)
+
+# Exercício 3 - Crie duas strings e concatene as duas em uma terceira string
+a = 'Concatenando'
+b= ' Strings'
+c= a+b
+print (c)
+
+# Exercício 4 - Crie uma tupla com os seguintes elementos: 1, 2, 2, 3, 4, 4, 4, 5 e depois utilize a função count do 
+# objeto tupla para verificar quantas vezes o número 4 aparece na tupla
+
+rep = (1, 2, 2, 3, 4, 4, 4, 5)
+print(rep.count(4))
+
+# Exercício 5 - Crie um dicionário vazio e imprima na tela
+dic ={}
+print (dic)
+
+dic_2 ={'arroz':['branco'],'amora':['roxo'], 'melancia':['verde'] }
+print (dic_2)
