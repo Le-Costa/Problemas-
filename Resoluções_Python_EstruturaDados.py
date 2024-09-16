3f6b62f39b88614280406606248e4fa18ef46b07
Leticia Costa <leticia.k.f.dacosta@gmail.com>
2024-09-16 15:09:20 -0300

    16092024_1508_finalizacao exercicios

--- Resoluções_Python_EstruturaDados.py
+++ Resoluções_Python_EstruturaDados.py
@@ -20,7 +20,35 @@ print(rep.count(4))
 
 # Exercício 5 - Crie um dicionário vazio e imprima na tela
 dic ={}
-print (dic)
+print ('dic vazio',dic)
 
-dic_2 ={'arroz':['branco'],'amora':['roxo'], 'melancia':['verde'] }
-print (dic_2)
+# Exercício 6 - Crie um dicionário com 3 chaves e 3 valores e imprima na tela
+dic_2 ={'arroz':'branco','amora':'roxo', 'melancia':'verde' }
+print ('dic2',dic_2)
+
+# Exercício 6 - Crie um dicionário com 3 chaves e 3 valores e imprima na tela
+dic_3 = {'bebida1': 'Coca cola', 'bebida2': 'Agua de coco', 'bebida3': 'Pepsi'}
+print ("dic3",dic_3)
+
+# Exercício 7 - Adicione mais um elemento ao dicionário criado no exercício anterior e imprima na tela
+dic_3 ['bebida4'] = 'fanta'
+print ('dic 3 alterado', dic_3)
+
+# Exercício 8 - Crie um dicionário com 3 chaves e 3 valores. 
+# Um dos valores deve ser uma lista de 2 elementos numéricos. 
+# Imprima o dicionário na tela.
+
+dic_0 = {'a':[23,25], b: 'Pedro', c:'Leticia'}
+print (dic_0)
+
+# Exercício 9 - Crie uma lista de 4 elementos. O primeiro elemento deve ser uma string, 
+# o segundo uma tupla de 2 elementos, o terceiro um dcionário com 2 chaves e 2 valores e 
+# o quarto elemento um valor do tipo float.
+# Imprima a lista na tela.
+lista_2 = [ "string 1", (5,6),{'fruta': 'banana', 'doce': 'banofee'},5.63   ]
+
+print (lista_2 )
+
+# Exercício 10 - Considere a string abaixo. Imprima na tela apenas os caracteres da posição 1 a 18.
+frase = 'Cientista de Dados é o profissional mais sexy do século XXI.'
+print (frase [0:18])
