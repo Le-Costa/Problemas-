2d3eea7187f7327ebddddcc26ca163bd7e569264
Leticia Costa <leticia.k.f.dacosta@gmail.com>
2024-10-23 16:36:52 -0300

    1636_23102024

--- lab3_Regex.py
+++ lab3_Regex.py
@@ -21,3 +21,25 @@ with open ("C:/Users/10391/Desktop/arquivos txt/Legiao urbana.txt",'r' ) as arqu
 resultado2= len (re.findall('tempo',musica) )
 print (resultado2)
 
+# 3- Crie um REGEX em Python para extrair as palavras seguidas por exclamação.
+with open ("C:/Users/10391/Desktop/arquivos txt/Legiao urbana.txt",'r' ) as arquivo: 
+    musica = arquivo.read()
+
+resultado3= (re.findall(r'\b\w+!',musica) )
+print (resultado3)
+
+# 4- Crie um REGEX que extrai qualquer palavra cujo antecessor seja a palavra "esse"
+# e o sucessor seja a palavra "amargo" em um texto.
+with open ("C:/Users/10391/Desktop/arquivos txt/Legiao urbana.txt",'r' ) as arquivo: 
+    musica = arquivo.read()
+    
+resultado3= (re.findall(r'\besse\s(\w+)\samargo\b', musica))
+print(resultado3)
+
+# 5- Crie um REGEX que retorne as palavras com acento, mas somente os caracteres na palavra que 
+# são anteriores ao caracter com acento.
+with open ("C:/Users/10391/Desktop/arquivos txt/Legiao urbana.txt",'r' ) as arquivo: 
+    musica = arquivo.read()
+    resultado5 = re.findall(r"(\w+)[\u00C0-\u017F]+", musica)
+    
+print(resultado5)
